f64d965aa01a2557bd753463a3ec310b27613e1f
rsakie <rsakie@yahoo.com>
2017-07-11 16:27:41 -0400

    tutoring

---
+++ Tutoring/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+      
+        {
+                int[] numberInArray = { 4, 5, 10, 12, 23 };
+                Sum(numberInArray);
+            }
+
+            static public void Sum(int[] myArray)
+        {
+            int sum = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                sum += myArray[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
