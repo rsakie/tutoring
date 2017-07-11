2162de0b4987344638cdecae1515e3e4f798e210
rsakie <rsakie@yahoo.com>
2017-07-11 17:10:21 -0400

    tutoring practice

--- Tutoring/Program.cs
+++ Tutoring/Program.cs
@@ -11,18 +11,23 @@ namespace Tutoring
         static void Main(string[] args)
       
         {
-                int[] numberInArray = { 4, 5, 10, 12, 23 };
-                Sum(numberInArray);
-            }
+            villians boxGhost = new villians();
+            villians badGuy2 = new villians("EHHH", "Street Cloths",75);
+            badGuy2.EvilPowers = badGuy2.GetPowers();
+            //Console.WriteLine(badGuy2.EvilPowers);
+            Console.WriteLine(badGuy2.GetPowers());
+        //        int[] numberInArray = { 4, 5, 10, 12, 23 };
+        //        Sum(numberInArray);
+        //    }
 
-            static public void Sum(int[] myArray)
-        {
-            int sum = 0;
-            for (int i = 0; i < myArray.Length; i++)
-            {
-                sum += myArray[i];
-            }
-            Console.WriteLine(sum);
+        //    static public void Sum(int[] myArray)
+        //{
+        //    int sum = 0;
+        //    for (int i = 0; i < myArray.Length; i++)
+        //    {
+        //        sum += myArray[i];
+        //    }
+        //    Console.WriteLine(sum);
         }
     }
 }
---
+++ Tutoring/villians.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutoring
+{
+    class villians
+    {
+        //fields
+        private string costumes;
+        private string name;
+        private string evilPowers;
+        private int strengthLevel;
+
+        //properties
+        public string Costumes { get; set; }
+        public string Name { get; set; }
+        public string EvilPowers { get; set; }
+        public int StrengthLevel { get; set; }
+
+        //contractors
+        public villians()
+        {
+            Random random = new Random();
+            this.costumes = " A bed sheet";
+            this.name = "A really bad dude";
+            this.evilPowers = "I can control empty boxes";
+            this.strengthLevel = random.Next(0, 51);
+        }
+
+        public villians(string name, string costumes, int strengthLevel)
+        {
+            this.name = name;
+            this.costumes = costumes;
+            this.evilPowers = GetPowers();
+            this.strengthLevel = strengthLevel;
+        }
+
+        //methods
+        public string GetPowers()
+        {
+            string[] powers = { " Killer Farts", "Mind Control", "Throwing Fire", "Words that can kill" };
+            {
+                Random random = new Random();
+                string newPower = powers[random.Next(0, powers.Length)];
+                return newPower;
+            }
+
+        }
+
+    }
+}
